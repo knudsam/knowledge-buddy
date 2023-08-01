c4ca89de3985c2175c1930c1e292be348f93d955
Jadyn Gomez <jadyngg19@gmail.com>
2023-08-01 19:23:11 -0400

    Getting rid of unnecessary files

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,31 +1,14 @@
- import Index from "views/Index.js";
+import Index from "views/Index.js";
 import Profile from "views/examples/Profile.js";
-import Maps from "views/examples/Maps.js";
 import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
-import Tables from "views/examples/Tables.js";
-import Icons from "views/examples/Icons.js";
 
 var routes = [
   {
     path: "/index",
-    name: "Dashboard",
+    name: "Home",
     icon: "ni ni-tv-2 text-primary",
     component: <Index />,
-    layout: "/admin",
-  },
-  {
-    path: "/icons",
-    name: "Icons",
-    icon: "ni ni-planet text-blue",
-    component: <Icons />,
-    layout: "/admin",
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    icon: "ni ni-pin-3 text-orange",
-    component: <Maps />,
     layout: "/admin",
   },
   {
@@ -33,13 +16,6 @@ var routes = [
     name: "User Profile",
     icon: "ni ni-single-02 text-yellow",
     component: <Profile />,
-    layout: "/admin",
-  },
-  {
-    path: "/tables",
-    name: "Tables",
-    icon: "ni ni-bullet-list-67 text-red",
-    component: <Tables />,
     layout: "/admin",
   },
   {
