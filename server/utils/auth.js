d2cdbce308c1f82cd66d5a7dec216f38ffeaed60
knudsam <knudsam@gmail.com>
2023-07-31 21:22:31 -0400

    Setup inital commit for auth.js and login/logout

---
+++ server/utils/auth.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const jwt = require('jsonwebtoken');
+
+const secret = proces.env.JWT_SECRET;
+const expiration = '2h';
+
+module.exports = {
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
