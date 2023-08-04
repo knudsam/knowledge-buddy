064a1bff6e4d4718519aa2614a0aa513db4dd70a
Jadyn Gomez <jadyngg19@gmail.com>
2023-08-03 20:33:20 -0400

    Putting back in the root package.json and fixing proxy key

--- client/src/graphql.js
+++ client/src/graphql.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 // Replace this URL with your actual GraphQL server URL
 const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql', // Adjust the URL as needed
+  uri: '/graphql', // Adjust the URL as needed
   cache: new InMemoryCache(),
 });
 
